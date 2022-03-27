5ca2f9d83a38b9120ccfa6d80557d66e072269cf
Eric Lehmann <lehm_eric@yahoo.fr>
2022-03-27 17:04:26 +0200

    desktop layout finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import divider from "./images/pattern-divider-mobile.svg";
+import dice from "./images/icon-dice.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [id, setId] = useState("");
+	const [advice, setAdvice] = useState("");
+
+	async function getData() {
+		let randomId = Math.floor(Math.random() * 220);
+		await fetch(`https://api.adviceslip.com/advice/${randomId}`)
+			.then((response) => response.json())
+			.then((data) => {
+				setId(data.slip.id);
+				setAdvice(data.slip.advice);
+			})
+			.catch((error) => console.log(error));
+	}
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	return (
+		<div className="App">
+			<div className="container">
+				<h1>Advice #{id}</h1>
+				<p>"{advice}"</p>
+				<img className="divider" src={divider} alt="" />
+				<div>
+					<button onClick={getData}>
+						<img src={dice} alt="" />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
